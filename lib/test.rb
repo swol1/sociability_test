3d1dc2d1ae72c0f187661b0cad49ce3ebb80b7b7
swol1 <den.gavrilin1@gmail.com>
2018-10-09 19:55:58 +0300

    initial commit

---
+++ lib/test.rb
@@ -0,0 +1,36 @@
+class Test
+  attr_reader :questions
+
+  def initialize(questions)
+    @answer_points = 0
+    @questions = questions
+  end
+
+  def scoring(user_choice)
+    case user_choice
+    when 1
+      @answer_points += 1
+    when 2
+      @answer_points += 2
+    end
+  end
+
+  def result_output(answers)
+    case @answer_points
+    when 0..3
+      answers[0]
+    when 4..8
+      answers[1]
+    when 9..13
+      answers[2]
+    when 14..18
+      answers[3]
+    when 19..24
+      answers[4]
+    when 25..29
+      answers[5]
+    when 30..32
+      answers[6]
+    end
+  end
+end
